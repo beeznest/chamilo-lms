8e3f65b09cd989bbd06a8206e22bf9a75d70d3aa
danilobrinu <ddbn.c2@gmail.com>
2014-09-24 11:09:36 -0500

    Add namespace "Chamilo" to composer and new models to User, Course, Section, QuizQuestion and TrackExercices

---
+++ dev/chamilo/src/Chamilo/Model/TrackExercices.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Chamilo\Model;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class TrackExercices extends Eloquent
+{
+	protected $table = 'track_e_exercices';
+}
+
+/* End of file StatisticTrackExercices.php */
+/* Location: .//var/www/html/chamilo-lms-utp/dev/models/StatisticTrackExercices.php */
